b57d7c15fc22c57973a878119928a4ea0badbf07
blackninja027 <65844128+blackninja027@users.noreply.github.com>
2024-01-24 15:51:51 +0530

    Update logger.py

--- sensor/logger.py
+++ sensor/logger.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path = os.path.join(from_root() , "logs", LOG_FILE)
 
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
